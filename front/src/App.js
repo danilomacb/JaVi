ab8a7104b886c076aaa71490ba4bdb30df8aa209
danilomacb <danilo.bakun@gmail.com>
2019-10-22 10:58:13 -0300

    creating components

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,20 @@
-import React from 'react';
-
-function App() {
-  return (
-    <h1>Hello World</h1>
-  );
+import React, { Component } from 'react';
+import { Link, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import Secret from './components/Secret';
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/secret">Secret</Link></li>
+        </ul>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/secret" component={Secret} />
+        </Switch>
+      </div>
+    );
+  }
 }
-
-export default App;
