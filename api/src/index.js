a788da1a6f20741aeb9f49a36fd27616b721a7fd
danilomacb <danilo.bakun@gmail.com>
2019-10-22 10:36:56 -0300

    connecting on mongoose and creating new routes

--- api/src/index.js
+++ api/src/index.js
@@ -1,7 +1,24 @@
-const express = require('express')
-const app = express()
-const port = 3001
+const express = require('express');
+const mongoose = require('mongoose');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const user = require('./User');
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+const app = express();
+
+const mongo_uri = 'mongodb://localhost/auth';
+
+mongoose.connect(mongo_uri, function(err) {
+    console.log(`Successfully connected to ${mongo_uri}`);
+});
+
+const port = 3001;
+
+app.get('/api/home', function(req, res) {
+    res.send('Welcome!');
+});
+
+app.get('/api/secret', function(req, res) {
+    res.send('The password is potato');
+});
+
+app.listen(port, () => console.log(`listening on port ${port}`));
