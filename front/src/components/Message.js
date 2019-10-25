27a9fbb0929834d8b323f72397f4e1d1b98f66bc
danilomacb <danilo.bakun@gmail.com>
2019-10-25 13:59:11 -0300

    changing messages

--- front/src/components/Message.js
+++ front/src/components/Message.js
@@ -2,10 +2,10 @@ import React from "react";
 
 function Message({ loginStatus }) {
   if (loginStatus === true) {
-    return <h1>Sucesso</h1>;
+    return <h1>Seja Bem Vindo</h1>;
   }
   if (loginStatus === false) {
-    return <h1>Erro</h1>;
+    return <h1>Erro ao Fazer Login</h1>;
   }
 
   return <></>;
