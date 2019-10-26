9a7d8d4c8849088012a7cbe0b0ee6475949dc8c3
danilomacb <danilo.bakun@gmail.com>
2019-10-26 10:53:02 -0300

    creating javi schema

---
+++ api/src/models/JaVi.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const JaViSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  type: {
+    type: String,
+    required: true
+  },
+  genre: {
+    type: String,
+    required: true
+  },
+  episodes: {
+    type: Number,
+    required: false
+  },
+  userEmail: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model("JaVi", JaViSchema);
