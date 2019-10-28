98ebdb11f16dd6412b49c14aa558f13a7361a888
danilomacb <danilo.bakun@gmail.com>
2019-10-28 15:45:19 -0300

    showing watcheds on front

--- front/src/components/Watcheds.js
+++ front/src/components/Watcheds.js
@@ -10,12 +10,21 @@ class Watcheds extends Component {
   }
 
   render() {
-    console.log(this.props.watcheds);
+    if (!this.props.watcheds || this.props.watcheds.length === 0) {
+      return (
+        <>
+          <Link to="/add-assistido">Adicionar Novo</Link>
+          <h1>Sua lista esta vazia</h1>
+        </>
+      );
+    }
 
     return (
       <>
         <Link to="/add-assistido">Adicionar Novo</Link>
-        <h1>Lista de assistidos</h1>
+        {this.props.watcheds.map((element, _id) => (
+          <h1 key={_id}>{element.name}</h1>
+        ))}
       </>
     );
   }
