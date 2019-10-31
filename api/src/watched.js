7ad035e0d47b00cf1fe2efad8e82be3d9a1db8c9
danilomacb <danilo.bakun@gmail.com>
2019-10-31 15:33:01 -0300

    protectiing watched routes

--- api/src/watched.js
+++ api/src/watched.js
@@ -6,7 +6,7 @@ const getEmail = require("./get-email");
 
 const router = express.Router();
 
-router.post("/add", (req, res) => {
+router.post("/add", withAuth, (req, res) => {
   const { name, type, genre, episode } = req.body;
   const userEmail = getEmail(req, res);
   const watched = new Watched({ name, type, genre, episode, userEmail });
@@ -20,19 +20,19 @@ router.post("/add", (req, res) => {
   });
 });
 
-router.get("/get/:id", async (req, res) => {
+router.get("/get/:id", withAuth, async (req, res) => {
   const id = req.params.id;
   const watched = await Watched.findById(id);
   res.status(200).json(watched);
 });
 
-router.get("/get-all", async (req, res) => {
+router.get("/get-all", withAuth, async (req, res) => {
   const userEmail = getEmail(req, res);
   const watcheds = await Watched.find({ userEmail });
   res.status(200).json(watcheds);
 });
 
-router.delete("/delete/:id", async (req, res) => {
+router.delete("/delete/:id", withAuth, async (req, res) => {
   const id = req.params.id;
   await Watched.findByIdAndDelete(id);
   res.status(200).send();
