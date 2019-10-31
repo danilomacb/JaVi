7fe66508ec77d3e0df50b4383d27e458ea7224af
danilomacb <danilo.bakun@gmail.com>
2019-10-31 13:55:38 -0300

    load watcheds after delete

--- front/src/sagas/deleteWatched.js
+++ front/src/sagas/deleteWatched.js
@@ -1,9 +1,13 @@
 import { all, call, takeEvery, put } from "redux-saga/effects";
-import { DELETE_WATCHED } from "../state/actions";
+import { DELETE_WATCHED, GET_WATCHEDS } from "../state/actions";
 
 function* deleteWatched(action) {
   const endpoint = "/watched/delete/" + action.id;
   const response = yield call(fetch, endpoint, { method: "DELETE" });
+
+  if (response.status === 200) {
+    yield put({ type: GET_WATCHEDS });
+  }
 }
 
 function* getAction() {
