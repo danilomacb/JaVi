9eca4fcc99e6ee6521d461f8ca8ad8662a62e715
danilomacb <danilo.bakun@gmail.com>
2019-11-06 09:48:20 -0300

    new collection name

--- api/src/models/Watched.js
+++ api/src/models/Watched.js
@@ -1,29 +1,32 @@
 const mongoose = require("mongoose");
 
-const WatchedSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
+const WatchedSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    type: {
+      type: String
+    },
+    genre: {
+      type: String
+    },
+    season: {
+      type: String
+    },
+    episode: {
+      type: String
+    },
+    comment: {
+      type: String
+    },
+    userEmail: {
+      type: String,
+      required: true
+    }
   },
-  type: {
-    type: String
-  },
-  genre: {
-    type: String
-  },
-  season: {
-    type: String
-  },
-  episode: {
-    type: String
-  },
-  comment: {
-    type: String
-  },
-  userEmail: {
-    type: String,
-    required: true
-  }
-});
+  { collection: "watched" }
+);
 
 module.exports = mongoose.model("Watched", WatchedSchema);
