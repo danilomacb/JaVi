1dfa730dcced1ee5ffdbffba378b952ad7772589
danilomacb <danilo.bakun@gmail.com>
2019-11-06 10:58:50 -0300

    ops i forgot this

--- front/src/App.js
+++ front/src/App.js
@@ -6,7 +6,7 @@ import UserForm from "./components/UserForm";
 import WithAuth from "./components/WithAuth";
 import Navigation from "./components/Navigation";
 import CardList from "./components/CardList";
-import WatchedForm from "./components/AddForm";
+import AddForm from "./components/AddForm";
 
 export default class App extends Component {
   render() {
@@ -18,11 +18,11 @@ export default class App extends Component {
           <Route path="/entrar" component={props => <UserForm {...props} title="Entrar" />} />
           <Route path="/cadastrar" component={props => <UserForm {...props} title="Cadastrar" />} />
           <Route path="/assistidos" component={WithAuth(CardList)} />
-          <Route path="/add-assistido" component={WithAuth(WatchedForm)} />
-          <Route path="/assistido/:id" component={WithAuth(WatchedForm)} />
+          <Route path="/add-assistido" component={WithAuth(AddForm)} />
+          <Route path="/assistido/:id" component={WithAuth(AddForm)} />
           <Route path="/para-assistir" component={WithAuth(CardList)} />
-          <Route path="/add-para-assistir" component={WithAuth(WatchedForm)} />
-          <Route path="/para-assistir/:id" component={WithAuth(WatchedForm)} />
+          <Route path="/add-para-assistir" component={WithAuth(AddForm)} />
+          <Route path="/para-assistir/:id" component={WithAuth(AddForm)} />
         </Switch>
       </div>
     );
