1d39e7cb5ffe018f8b746204967bd0a0191ee2bb
danilomacb <danilo.bakun@gmail.com>
2019-11-06 10:46:24 -0300

    Wacheds now is CardList

--- front/src/App.js
+++ front/src/App.js
@@ -5,7 +5,7 @@ import Home from "./components/Home";
 import UserForm from "./components/UserForm";
 import WithAuth from "./components/WithAuth";
 import Navigation from "./components/Navigation";
-import Watcheds from "./components/Watcheds";
+import CardList from "./components/CardList";
 import WatchedForm from "./components/WatchedForm";
 
 export default class App extends Component {
@@ -17,10 +17,10 @@ export default class App extends Component {
           <Route path="/" exact component={Home} />
           <Route path="/entrar" component={props => <UserForm {...props} title="Entrar" />} />
           <Route path="/cadastrar" component={props => <UserForm {...props} title="Cadastrar" />} />
-          <Route path="/assistidos" component={WithAuth(Watcheds)} />
+          <Route path="/assistidos" component={WithAuth(CardList)} />
           <Route path="/add-assistido" component={WithAuth(WatchedForm)} />
           <Route path="/assistido/:id" component={WithAuth(WatchedForm)} />
-          <Route path="/para-assistir" component={WithAuth(Watcheds)} />
+          <Route path="/para-assistir" component={WithAuth(CardList)} />
           <Route path="/add-para-assistir" component={WithAuth(WatchedForm)} />
           <Route path="/para-assistir/:id" component={WithAuth(WatchedForm)} />
         </Switch>
