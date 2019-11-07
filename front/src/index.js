f744d379a539b39f9058573e98f6499a5ba20a93
danilomacb <danilo.bakun@gmail.com>
2019-11-07 09:54:55 -0300

    creating store file

--- front/src/index.js
+++ front/src/index.js
@@ -1,52 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
-import createSagaMiddleware from "redux-saga";
-import { createBrowserHistory } from "history";
-import { routerMiddleware, ConnectedRouter } from "connected-react-router";
+import { ConnectedRouter } from "connected-react-router";
 
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from "./App";
-import createRootReducer from "./state/reducers";
-import runAddWatched from "./sagas/addWatched";
-import runAddUser from "./sagas/addUser";
-import runLogin from "./sagas/login";
-import runGetWatcheds from "./sagas/getWatcheds";
-import runCheckToken from "./sagas/checkToken";
-import runDeleteWatched from "./sagas/deleteWatched";
-import runGetWatched from "./sagas/getWatched";
-import runUpdateWatched from "./sagas/updateWatched";
-import runGetToWatchList from "./sagas/getToWatchList";
-import runAddToWatch from "./sagas/addToWatch";
-import runGetToWatch from "./sagas/getToWatch";
-import runUpdateToWatch from "./sagas/updateToWatch";
-import runDeleteToWatch from "./sagas/deleteToWatch";
+import configureStore, { history } from "./state/store";
 
-const history = createBrowserHistory();
-
-const sagaMiddleware = createSagaMiddleware();
-
-const store = createStore(
-  createRootReducer(history),
-  applyMiddleware(routerMiddleware(history), sagaMiddleware)
-);
-
-sagaMiddleware.run(runAddWatched);
-sagaMiddleware.run(runAddUser);
-sagaMiddleware.run(runLogin);
-sagaMiddleware.run(runGetWatcheds);
-sagaMiddleware.run(runCheckToken);
-sagaMiddleware.run(runDeleteWatched);
-sagaMiddleware.run(runGetWatched);
-sagaMiddleware.run(runUpdateWatched);
-sagaMiddleware.run(runGetToWatchList);
-sagaMiddleware.run(runAddToWatch);
-sagaMiddleware.run(runGetToWatch);
-sagaMiddleware.run(runUpdateToWatch);
-sagaMiddleware.run(runDeleteToWatch);
+const store = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
---
+++ front/src/state/store.js
@@ -0,0 +1,46 @@
+import { createStore, applyMiddleware } from "redux";
+import createSagaMiddleware from "redux-saga";
+import { routerMiddleware } from "connected-react-router";
+import { createBrowserHistory } from "history";
+
+import createRootReducer from "./reducers";
+import runAddWatched from "../sagas/addWatched";
+import runAddUser from "../sagas/addUser";
+import runLogin from "../sagas/login";
+import runGetWatcheds from "../sagas/getWatcheds";
+import runCheckToken from "../sagas/checkToken";
+import runDeleteWatched from "../sagas/deleteWatched";
+import runGetWatched from "../sagas/getWatched";
+import runUpdateWatched from "../sagas/updateWatched";
+import runGetToWatchList from "../sagas/getToWatchList";
+import runAddToWatch from "../sagas/addToWatch";
+import runGetToWatch from "../sagas/getToWatch";
+import runUpdateToWatch from "../sagas/updateToWatch";
+import runDeleteToWatch from "../sagas/deleteToWatch";
+
+export const history = createBrowserHistory();
+
+export default function configureStore() {
+  const sagaMiddleware = createSagaMiddleware();
+
+  const store = createStore(
+    createRootReducer(history),
+    applyMiddleware(routerMiddleware(history), sagaMiddleware)
+  );
+
+  sagaMiddleware.run(runAddWatched);
+  sagaMiddleware.run(runAddUser);
+  sagaMiddleware.run(runLogin);
+  sagaMiddleware.run(runGetWatcheds);
+  sagaMiddleware.run(runCheckToken);
+  sagaMiddleware.run(runDeleteWatched);
+  sagaMiddleware.run(runGetWatched);
+  sagaMiddleware.run(runUpdateWatched);
+  sagaMiddleware.run(runGetToWatchList);
+  sagaMiddleware.run(runAddToWatch);
+  sagaMiddleware.run(runGetToWatch);
+  sagaMiddleware.run(runUpdateToWatch);
+  sagaMiddleware.run(runDeleteToWatch);
+
+  return store;
+}
