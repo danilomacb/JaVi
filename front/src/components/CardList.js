11ad47162bf6ee3c3023c88f527cb0335e5573d4
danilomacb <danilo.bakun@gmail.com>
2019-11-07 08:57:08 -0300

    title for card list

--- front/src/components/CardList.js
+++ front/src/components/CardList.js
@@ -26,65 +26,63 @@ class Watcheds extends Component {
       tempList = this.props.toWatchList;
     }
 
-    if (!tempList || tempList.length === 0) {
-      return (
-        <div className="my-container">
-          <Link to="/add-assistido">Adicionar Novo</Link>
-          <h1>Sua lista esta vazia</h1>
-        </div>
-      );
-    }
-
     return (
       <>
         <Message />
         <div className="my-container">
+          <h1 className="text-center">
+            {this.props.match.path === "/assistidos" ? "Assistidos" : "Para Assistir"}
+          </h1>
           <Link
             to={this.props.match.path === "/assistidos" ? "/add-assistido" : "/add-para-assistir"}
           >
             <div className="my-button mb-4 text-center">Adicionar Novo</div>
           </Link>
-          <Row>
-            {tempList.map((temp, _id) => (
-              <Col key={_id} xs={12} sm={6} lg={4} className="mb-4">
-                <Card className="my-card">
-                  <Card.Header className="my-card-header">{temp.name}</Card.Header>
-                  <Card.Body>
-                    <Card.Text>
-                      Tipo: {temp.type} <br />
-                      Gênero: {temp.genre} <br />
-                      {temp.episode ? "Episódio: " + temp.episode : null}
-                    </Card.Text>
-                    <ButtonGroup className="w-100">
-                      {this.props.match.path === "/assistidos" ? (
-                        <Link className="btn my-button" to={"/assistido/" + temp._id}>
-                          Editar
-                        </Link>
-                      ) : (
-                        <Link className="btn my-button" to={"/para-assistir/" + temp._id}>
-                          Editar
-                        </Link>
-                      )}
+          {tempList && tempList.length > 0 ? (
+            <Row>
+              {tempList.map((temp, _id) => (
+                <Col key={_id} xs={12} sm={6} lg={4} className="mb-4">
+                  <Card className="my-card">
+                    <Card.Header className="my-card-header">{temp.name}</Card.Header>
+                    <Card.Body>
+                      <Card.Text>
+                        Tipo: {temp.type} <br />
+                        Gênero: {temp.genre} <br />
+                        {temp.episode ? "Episódio: " + temp.episode : null}
+                      </Card.Text>
+                      <ButtonGroup className="w-100">
+                        {this.props.match.path === "/assistidos" ? (
+                          <Link className="btn my-button" to={"/assistido/" + temp._id}>
+                            Editar
+                          </Link>
+                        ) : (
+                          <Link className="btn my-button" to={"/para-assistir/" + temp._id}>
+                            Editar
+                          </Link>
+                        )}
 
-                      <button
-                        className="btn my-button"
-                        onClick={() => {
-                          if (this.props.match.path === "/assistidos") {
-                            this.props.dispatch(deleteWatched(temp._id));
-                          }
-                          if (this.props.match.path === "/lista-para-assistir") {
-                            this.props.dispatch(deleteToWatch(temp._id));
-                          }
-                        }}
-                      >
-                        Deletar
-                      </button>
-                    </ButtonGroup>
-                  </Card.Body>
-                </Card>
-              </Col>
-            ))}
-          </Row>
+                        <button
+                          className="btn my-button"
+                          onClick={() => {
+                            if (this.props.match.path === "/assistidos") {
+                              this.props.dispatch(deleteWatched(temp._id));
+                            }
+                            if (this.props.match.path === "/lista-para-assistir") {
+                              this.props.dispatch(deleteToWatch(temp._id));
+                            }
+                          }}
+                        >
+                          Deletar
+                        </button>
+                      </ButtonGroup>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              ))}
+            </Row>
+          ) : (
+            <p>Sua lista esta vazia</p>
+          )}
         </div>
       </>
     );
