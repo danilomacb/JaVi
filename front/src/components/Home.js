065a21d4dedd9d837fe83c6ea59abf5333cca1e7
danilomacb <danilo.bakun@gmail.com>
2019-11-12 13:35:56 -0300

    update on home message

--- front/src/components/Home.js
+++ front/src/components/Home.js
@@ -7,12 +7,20 @@ function Home() {
     <>
       <Message />
       <div className="my-container">
-        <h1 className="text-center">Afinal, pra que serve isso?</h1>
+        <h1 className="text-center">Seja Bem Vindo!</h1>
         <p>
-          Você já se perguntou se já viu um filme, ou um episódio de série/anime? Com diferentes
-          plataformas de streaming fica difícil lembrar de todos eles não é mesmo? A proposta do
-          JaVi?! é deixar salvo em uma única plataforma o que você já assistiu, ou que quer assistir
-          depois, de forma simples e prática.
+          O JaVi?! é uma plataforma que tem como objetivo deixar salvo de forma simples e prática
+          quais conteúdos você ja viu, e quais você quer ver, em um único lugar. Sejam eles filmes,
+          séries, animes, ou o que mais você desejar.
+        </p>
+        <h1 className="text-center mt-5">Como Utilizar?</h1>
+        <p>
+          Primeiro você precisa criar uma conta, depois fazer login, logo após irão aparecer no menu
+          de cima as opções para salvar conteúdos que já foram assistidos e conteúdos para assistir
+          depois. Clicando em qualquer uma delas irá aparecer na tela uma opção para adcionar
+          conteúdo. É importante ressaltar que somente o nome é obrigatório, os campos restantes
+          podem ficar vazios. Todos os campos aceitam tanto caracteres numéricos quanto alfabéticos,
+          sinta-se livre para deixar salvo da forma que achar melhor.
         </p>
       </div>
     </>
