24e89a1c3258ed1810a5431460daaf02c325db4f
danilomacb <danilo.bakun@gmail.com>
2019-11-12 14:26:27 -0300

    icons on navbar

--- front/src/components/Navigation.js
+++ front/src/components/Navigation.js
@@ -2,6 +2,16 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { Navbar, Nav } from "react-bootstrap";
 import { connect } from "react-redux";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHome,
+  faCheck,
+  faList,
+  faSignInAlt,
+  faSignOutAlt,
+  faUserPlus
+} from "@fortawesome/free-solid-svg-icons";
+import { faEye } from "@fortawesome/free-regular-svg-icons";
 
 import "../styles/navigation.css";
 import { checkToken, resetToken } from "../state/actions/auth";
@@ -15,20 +25,24 @@ class Navigation extends Component {
     return (
       <Navbar collapseOnSelect expand="md" className="my-nav" variant="dark">
         <Link to="/" className="navbar-brand">
+          <FontAwesomeIcon icon={faEye} className="mr-1" />
           JaVi?!
         </Link>
         <Navbar.Toggle aria-controls="navigation" />
         <Navbar.Collapse id="navigation">
           <Nav className="mr-auto">
             <Link to="/" className="nav-link">
+              <FontAwesomeIcon icon={faHome} className="mr-1" />
               Início
             </Link>
             {this.props.token ? (
               <>
                 <Link to="/lista-de-assistidos" className="nav-link">
+                  <FontAwesomeIcon icon={faCheck} className="mr-1" />
                   Assistidos
                 </Link>
                 <Link to="/lista-para-assistir" className="nav-link">
+                  <FontAwesomeIcon icon={faList} className="mr-1" />
                   Para Assistir
                 </Link>
               </>
@@ -43,14 +57,17 @@ class Navigation extends Component {
                   return (document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC;");
                 }}
               >
+                <FontAwesomeIcon icon={faSignOutAlt} className="mr-1" />
                 Sair
               </Link>
             ) : (
               <>
                 <Link to="/cadastrar" className="nav-link">
+                  <FontAwesomeIcon icon={faUserPlus} className="mr-1" />
                   Cadastrar
                 </Link>
                 <Link to="/entrar" className="nav-link">
+                  <FontAwesomeIcon icon={faSignInAlt} className="mr-1" />
                   Entrar
                 </Link>
               </>
